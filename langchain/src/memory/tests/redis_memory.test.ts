4a3c5f8743a9a6746deaf4853b12c816fb96e0db
Chris Toomey <ctoomey@fb.com>
2023-04-19 08:48:29 -0700

    Updated tests for memory return option

--- langchain/src/memory/tests/redis_memory.test.ts
+++ langchain/src/memory/tests/redis_memory.test.ts
@@ -1,27 +1,13 @@
-import { test, expect, jest } from "@jest/globals";
+import { test, expect } from "@jest/globals";
 import { RedisMemory } from "../redis_memory.js";
-// import { HumanChatMessage, AIChatMessage } from "../../schema/index.js";
+import { HumanChatMessage, AIChatMessage } from "../../schema/index.js";
 import { createClient } from "redis";
 
-// TODO Add test for returning with messages
 // TODO Update docs
-test("Test Redis memory", async () => {
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () =>
-        Promise.resolve({
-          messages: [
-            { role: "AI", content: "Ozzy Osbourne" },
-            { role: "Human", content: "Who is the best vocalist?" },
-          ],
-        }),
-    } as Response)
-  );
-
+test("Test Redis memory without messages", async () => {
   const client = createClient();
   //@ts-ignore foo
   const memory = new RedisMemory(client, {
-    // returnMessages: true,
     sessionId: "one",
   });
   await memory.init();
@@ -35,32 +21,45 @@ test("Test Redis memory", async () => {
   const expectedString = "Human: Who is the best vocalist?\nAI: Ozzy Osbourne";
   const result2 = await memory.loadMemoryVariables({});
   expect(result2).toStrictEqual({ history: expectedString });
+  await client.flushDb();
 });
 
+test("Test Redis memory with messages", async () => {
+  const client = createClient();
+  //@ts-ignore foo
+  const memory = new RedisMemory(client, {
+    returnMessages: true,
+    sessionId: "two",
+  });
+  await memory.init();
+  const result1 = await memory.loadMemoryVariables({});
+  expect(result1).toStrictEqual({ history: [] });
+
+  await memory.saveContext(
+    { input: "Who is the best vocalist?" },
+    { response: "Ozzy Osbourne" }
+  );
+  const expectedHuman = new HumanChatMessage("Who is the best vocalist?");
+  const expectedAI = new AIChatMessage("Ozzy Osbourne");
+  const result2 = await memory.loadMemoryVariables({});
+  expect(result2).toStrictEqual({ history: [expectedHuman, expectedAI] });
+  await client.flushDb();
+});
+
+// TODO Add preloading function
 // test("Test Redis memory with pre-loaded history", async () => {
 //   const pastMessages = [
 //     new AIChatMessage("Nice to meet you, Ozzy!"),
 //     new HumanChatMessage("My name is Ozzy"),
 //   ];
-
-//   global.fetch = jest.fn(() =>
-//     Promise.resolve({
-//       json: () =>
-//         Promise.resolve({
-//           messages: [
-//             { role: "AI", content: "Nice to meet you, Ozzy!" },
-//             { role: "Human", content: "My name is Ozzy" },
-//           ],
-//         }),
-//     } as Response)
-//   );
 //   const client = createClient();
 //   //@ts-ignore foo
 //   const memory = new RedisMemory(client, {
 //     returnMessages: true,
-//     sessionId: "2",
+//     sessionId: "three",
 //   });
 //   await memory.init();
 //   const result = await memory.loadMemoryVariables({});
 //   expect(result).toStrictEqual({ history: pastMessages });
+//   await client.flushDb();
 // });
